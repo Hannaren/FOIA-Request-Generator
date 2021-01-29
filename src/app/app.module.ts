efc135b2307dbe7f54b7549a262a1fbd8245e591
Hannaren <sumpterhanna@gmail.com>
2021-01-28 22:34:36 -0600

    initial commit, form blocked out

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,17 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { ReactiveFormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
+import { LetterFormComponent } from './letter-form/letter-form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LetterFormComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
